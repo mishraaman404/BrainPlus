6eec7af8ccfcc644163c7e28639bcee03d3b0ca8
Mishaman-cyber <amishra52466@gmail.com>
2025-10-29 17:53:12 +0530

    Updated project files

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ document.addEventListener('DOMContentLoaded', () => {
         navLinks.classList.toggle('active');
     });
 });
+
+
+
+// Select modal
+const modal = document.getElementById("courseModal");
+const closeBtn = document.querySelector(".close-btn");
+
+// Open modal on Learn More click
+document.querySelectorAll(".learn-more-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    modal.style.display = "flex";
+    document.body.style.overflow = "hidden"; // prevent page scroll
+  });
+});
+
+// Close modal
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+  document.body.style.overflow = "auto";
+});
+
+// Close modal when clicking outside the box
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+});
+
